50edd8ae39fc317238948c99be5e8b3a9e823bc1
Manoj <manoj.gupta16539@sakec.ac.in>
2024-06-27 00:19:12 +0530

    Created submission form and completed CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <div className='header'>
+        <h1>Submission Form</h1>
+        </div>
+
+        <div className='form'>
+          <form>
+              <label>First Name*</label>
+              <input type="text" placeholder="Enter your First name"   required/>
+              <label>Last Name*</label>
+              <input type="text" placeholder="Enter your First name"   required />
+
+              <label>Email*</label>
+              <input type="email" placeholder="Enter your email"   required/>
+              <label>Contact*</label>
+              <input type="tel" placeholder="Enter your phone number"   required/>
+
+              <label>Gender*</label>
+              <select>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="other">Other</option>
+              </select>
+
+              <label>Upload Resume*</label>
+              <input type="file"  name="file" id="file" placeholder="Enter Upload File" required/>
+
+              <label>Enter Url*</label>
+              <input type="url" placeholder="Enter your Url"    required/>
+              <label>About*</label>
+              <textarea placeholder="Enter your About"  required/>
+            <div className='button'>
+            <button type="submit" value="Submit">Submit </button>
+            <button type='reset' value="reset" >Reset</button>
+            </div>
+          </form>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
